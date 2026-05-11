6c56268fb42f7e41fa08687fd310465181753e52
chaushmohammad <chaushmohammad389@gmail.com>
2026-05-11 12:59:50 +0530

    some

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,48 +1,176 @@
+// const cartContainer = document.getElementById("cart-products");
+// const cart = JSON.parse(localStorage.getItem("cart")) || [];
+// const cartCount = document.getElementById("cart-count");
+
+// cartCount.innerText = cart.length;
+
+// cartContainer.innerHTML = "";
+
+// const groupedCart = {};
+
+// cart.forEach((product) => {
+//     if (groupedCart[product.name]) {
+//         groupedCart[product.name].quantity++;
+//     } else {
+//         groupedCart[product.name] = {
+//             ...product,
+//             quantity: 1
+//         };
+//     }
+// });
+
+// let grandTotal = 0;
+
+// Object.values(groupedCart).forEach((product) => {
+//     const itemTotal = product.price * product.quantity;
+//     grandTotal += itemTotal;
+
+//     cartContainer.innerHTML += `
+//         <div class="cart-card">
+//             <img src="${product.image}" alt="${product.name}">
+
+//             <div>
+//                 <h3>${product.name}</h3>
+//                 <p>${product.description}</p>
+
+//                 <p><b>One item price:</b> ₹${product.price}</p>
+//                 <p><b>Quantity:</b> ${product.quantity}</p>
+//                 <h4>Total: ₹${itemTotal}</h4>
+//                 <button onclick="removeItem('${product.name}')">
+//                       ❌ Remove
+//                 </button>
+//             </div>
+//         </div>
+//     `;
+// });
+
+// cartContainer.innerHTML += `
+//     <div class="cart-total">
+//         <h2>Grand Total: ₹${grandTotal}</h2>
+//     </div>
+// `;
+
+
+
+
+// function removeItem(productName) {
+
+//     const itemIndex = cart.findIndex((item) =>
+//         item.name === productName
+//     );
+
+//     if (itemIndex !== -1) {
+
+//         cart.splice(itemIndex, 1);
+
+//         localStorage.setItem("cart", JSON.stringify(cart));
+
+//         location.reload();
+//     }
+// }
+
+
+
+
 const cartContainer = document.getElementById("cart-products");
-const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
 const cartCount = document.getElementById("cart-count");
 
-cartCount.innerText = cart.length;
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+
+if (cartCount) {
+    cartCount.innerText = cart.length;
+}
+
 
 cartContainer.innerHTML = "";
 
-const groupedCart = {};
 
-cart.forEach((product) => {
-    if (groupedCart[product.name]) {
-        groupedCart[product.name].quantity++;
-    } else {
-        groupedCart[product.name] = {
-            ...product,
-            quantity: 1
-        };
-    }
-});
+const groupedCart = {};
 
 let grandTotal = 0;
 
+
+cart.forEach((product) => {
+
+    if (groupedCart[product.name]) {
+
+        groupedCart[product.name].quantity++;
+
+    } else {
+
+        groupedCart[product.name] = {
+
+            ...product,
+            quantity: 1
+
+        };
+    }
+
+});
+
+
 Object.values(groupedCart).forEach((product) => {
+
     const itemTotal = product.price * product.quantity;
+
     grandTotal += itemTotal;
 
+
     cartContainer.innerHTML += `
+
         <div class="cart-card">
+
             <img src="${product.image}" alt="${product.name}">
 
             <div>
+
                 <h3>${product.name}</h3>
+
                 <p>${product.description}</p>
 
                 <p><b>One item price:</b> ₹${product.price}</p>
+
                 <p><b>Quantity:</b> ${product.quantity}</p>
+
                 <h4>Total: ₹${itemTotal}</h4>
+
+                <button onclick="removeItem('${product.name}')">
+                    Remove 1 Item
+                </button>
+
             </div>
+
         </div>
+
     `;
 });
 
+
 cartContainer.innerHTML += `
+
     <div class="cart-total">
+
         <h2>Grand Total: ₹${grandTotal}</h2>
+
     </div>
+
 `;
+
+
+function removeItem(productName) {
+
+    const itemIndex = cart.findIndex((item) =>
+        item.name === productName
+    );
+
+    if (itemIndex !== -1) {
+
+        cart.splice(itemIndex, 1);
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+
+        location.reload();
+    }
+}
--- assets/js/products.js
+++ assets/js/products.js
@@ -1,42 +1,83 @@
+// const container = document.getElementById("products-container");
+
+// function displayProducts(items) {
+//     container.innerHTML = "";
+
+//     items.forEach((product, index) => {
+//         container.innerHTML += `
+//             <div class="product-card">
+//                 <img src="${product.image}" alt="${product.name}">
+//                 <h3>${product.name}</h3>
+//                 <p>${product.description}</p>
+//                 <h4>₹${product.price}</h4>
+
+
+// <button onclick='addToCart("${product.name}")'>
+//     Add To Cart
+// </button>
+//             </div>
+//         `;
+//     });
+// }
+
+// displayProducts(products);
+
+// function filterProducts(category) {
+//     if (category === "all") {
+//         displayProducts(products);
+//     } else {
+//         const filteredProducts = products.filter((product) => {
+//             return product.category === category;
+//         });
+
+//         displayProducts(filteredProducts);
+//     }
+// }
+// const cartCount = document.getElementById("cart-count");
+
+// let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+// function updateCartCount() {
+//     cartCount.innerText = cart.length;
+// }
+
+// function addToCart(productName) {
+//     const product = products.find((item) =>
+//         item.name === productName
+//     );
+
+//     cart.push(product);
+
+// }
+
+// localStorage.setItem("cart", JSON.stringify(cart));
+
+// updateCartCount();
+
+
 const container = document.getElementById("products-container");
-
-function displayProducts(items) {
-    container.innerHTML = "";
-
-    items.forEach((product) => {
-        container.innerHTML += `
-            <div class="product-card">
-                <img src="${product.image}" alt="${product.name}">
-                <h3>${product.name}</h3>
-                <p>${product.description}</p>
-                <h4>₹${product.price}</h4>
-                <button onclick='addToCart("${product.name}")'>Add to Cart</button>
-            </div>
-        `;
-    });
-}
-
-displayProducts(products);
-
-function filterProducts(category) {
-    if (category === "all") {
-        displayProducts(products);
-    } else {
-        const filteredProducts = products.filter((product) => {
-            return product.category === category;
-        });
-
-        displayProducts(filteredProducts);
-    }
-}
 const cartCount = document.getElementById("cart-count");
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 function updateCartCount() {
-    cartCount.innerText = cart.length;
+    if (cartCount) {
+        cartCount.innerText = cart.length;
+    }
 }
 
+// function addToCart(productName) {
+//     const product = products.find((item) => item.name === productName);
+
+//     if (!product) {
+//         console.log("Product not found");
+//         return;
+//     }
+
+//     cart.push(product);
+//     localStorage.setItem("cart", JSON.stringify(cart));
+//     updateCartCount();
+// }
 function addToCart(productName) {
 
     const product = products.find((item) =>
@@ -49,7 +90,47 @@ function addToCart(productName) {
 
     updateCartCount();
 
-    alert(product.name + " added to cart!");
+    const message = document.getElementById("cart-message");
+
+    message.classList.add("show");
+
+    setTimeout(() => {
+
+        message.classList.remove("show");
+
+    }, 2000);
 }
 
+function displayProducts(items) {
+    container.innerHTML = "";
+
+    items.forEach((product) => {
+        container.innerHTML += `
+            <div class="product-card">
+                <img src="${product.image}" alt="${product.name}">
+                <h3>${product.name}</h3>
+                <p>${product.description}</p>
+                <h4>₹${product.price}</h4>
+
+                <button onclick='addToCart("${product.name}")'>
+                    Add To Cart
+                </button>
+            </div>
+        `;
+    });
+}
+
+function filterProducts(category) {
+    if (category === "all") {
+        displayProducts(products);
+    } else {
+        const filteredProducts = products.filter((product) => {
+            return product.category === category;
+        });
+
+        displayProducts(filteredProducts);
+    }
+}
+
+displayProducts(products);
 updateCartCount();
